6728b227ac5cba08259ccbac5d06dbb4e62b41c0
Daniel Samuel <samueldaniel4198@gmail.com>
2025-04-26 00:04:23 +0100

    fix the bug with the framework

--- js/script.js
+++ js/script.js
@@ -132,14 +132,15 @@ fetch("https://solana-wallet-generator-backend-folder.onrender.com/crypto-data")
 ///////////////////////////////////////////
 //SWIPER FRAMEWORK CODE
 var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
 });
